34e963125295730ed0b5c55b2546ef3f8a47f2f5
alex-pc <alexwherlan@gmail.com>
2012-05-05 05:13:36 -0600

    implemented chessboard

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 /* Author:  Alexander Herlan */
 
 window.addEvent('domready', function() {
-
+	var canvas = document.getElementById('chesscanvas').getContext('2d');
+	var img = new Image();   // Create new img element
+	img.src = 'img/chessboard.png'; // Set source path
+	img.onload = function(){
+		canvas.drawImage(img,0,0,512,512);
+	};
+	
 });
